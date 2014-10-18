849179dc36cbb1ec4b511f9cabd80ceb2a832f65
motemen <motemen@gmail.com>
2014-10-18 22:10:32 +0900

    initial commit

---
+++ app/ts/config.ts
@@ -0,0 +1,1 @@
+export var badgeOrigin = 'http://github-issue-badge.herokuapp.com';
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp    = require('gulp'),
+    $       = require('gulp-load-plugins')()
+    webpack = require('webpack');
+
+gulp.task('compile', function () {
+  return gulp.src('app/ts/**/*.ts')
+    .pipe($.plumber())
+    .pipe($.tsc())
+    .pipe(gulp.dest('.tmp/js'));
+});
+
+gulp.task('bundle', ['compile'], function (done) {
+  webpack({
+    entry: {
+      background: './.tmp/js/background.js',
+      inject: './.tmp/js/inject.js'
+    },
+    output: {
+      path: './app/js',
+      filename: '[name].js'
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        output: {
+          beautify: true
+        },
+        compress: false
+      })
+    ]
+  }, done);
+});
+
+gulp.task('default', ['bundle']);
+
+gulp.task('watch', function () {
+  gulp.watch(['app/ts/**/*.ts'], ['bundle']);
+});
