c440c7c33e0a2c0e3c710a270c6f5516697bc602
hatz48 <hatz48@hatena.ne.jp>
2017-03-06 23:12:31 +0900

    設定でおもてなし

--- src/ts/options.ts
+++ src/ts/options.ts
@@ -1,10 +1,21 @@
+function getConfig(elm: any) {
+    const origin  = elm.querySelector('[name="origin"]').value;
+    const apiRoot = elm.querySelector('[name="api-root"]').value;
+    const token   = elm.querySelector('[name="token"]').value;
+
+    return origin && apiRoot && token ? {
+        origin  : origin,
+        apiRoot : apiRoot,
+        token   : token
+    } : undefined;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const configs: any[] = JSON.parse(localStorage.getItem('origins') || '[]');
     configs.forEach(config => {
         if (config.origin === 'https://github.com') {
             if (!config.token) { return }
             const elm: any = document.querySelector('.github-config')
-            console.log(config)
             elm.querySelector('[name="token"]').value = config.token
         } else {
             if (!(config.origin && config.apiRoot && config.token)) { return }
@@ -15,31 +26,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-    function githubConfig() {
-        const githubToken: any = document.querySelector('.github-config [name="token"]');
-        const token = githubToken.value;
-        return token ? {
-            origin  : 'https://github.com',
-            apiRoot : 'https://api.github.com',
-            token   : token
-        } : undefined;
-    }
-    function githubEnterpriseConfig() {
-        const config: any = document.querySelector('.github-enterprise-config');
-        const origin = config.querySelector('[name="origin"]').value;
-        const apiRoot = config.querySelector('[name="api-root"]').value;
-        const token = config.querySelector('[name="token"]').value;
-        return origin && apiRoot && token ? {
-            origin  : origin,
-            apiRoot : apiRoot,
-            token   : token
-        } : undefined;
-    }
     document.querySelector('.save-button').addEventListener('click', function(e) {
-        const github = githubConfig();
-        const githubEnterprise = githubEnterpriseConfig();
+        const github = getConfig(document.querySelector('.github-config'));
+        const githubEnterprise = getConfig(document.querySelector('.github-enterprise-config'));
 
         const configs = [github, githubEnterprise].filter(c => c !== undefined)
         localStorage.setItem('origins', JSON.stringify(configs))
+    })
+
+    Array.prototype.forEach.call(document.querySelectorAll('.test-button'), (button: HTMLButtonElement) => {
+        button.addEventListener('click', function(e: any) {
+            const config = getConfig(e.target.parentNode);
+            if (config === undefined) {
+                alert('please input')
+                return;
+            }
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", config.apiRoot);
+            xhr.setRequestHeader("Authorization", `token ${config.token}`)
+            xhr.onload  = function(e) {
+                alert('connection ' + xhr.statusText + ': ' + xhr.responseText)
+            }
+            xhr.onerror = function(e) {
+                alert('connection failed')
+            }
+            xhr.send();
+        })
     })
 })
